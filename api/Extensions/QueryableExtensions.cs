6d8067d4f01e23f10b365a5af90db13e4276d67e
Tommy <tommycrabtree@outlook.com>
2021-06-17 03:19:11 -0500

    End of Section 18

---
+++ api/Extensions/QueryableExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using api.Entities;
+
+namespace api.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<Message> MarkUnreadAsRead(this IQueryable<Message> query, string currentUsername)
+        {
+            var unreadMessages = query.Where(m => m.DateRead == null
+                && m.RecipientUsername == currentUsername);
+            
+            if (unreadMessages.Any())
+            {
+                foreach (var message in unreadMessages)
+                {
+                    message.DateRead = DateTime.UtcNow;
+                }
+            }
+
+            return query;
+        }
+    }
+}
